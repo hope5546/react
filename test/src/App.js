1db513db361c95a51af44a871f9ee014c63a9696
hope5546 <hope5547@hotmail.com>
2017-09-12 20:55:03 +0700

    add homework

--- test/src/App.js
+++ test/src/App.js
@@ -4,6 +4,9 @@ import './App.css';
 import Header from './Components/Header.component.js';
 import Footer from './Components/Footer.component.js';
 import Calculator from './Components/Calculator.component.js';
+import { BrowserRouter,Switch,Route,NavLink} from 'react-router-dom';
+import {Home,Input,Output} from './Components';
+import {queryString} from 'query-string';
 class App extends Component {
 
   constructor(){
@@ -33,14 +36,18 @@ class App extends Component {
      ];
 
     return (
+      <BrowserRouter>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            <li><NavLink to="/" activeclassName="active">Home</NavLink></li>
+          </ul>
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
+        <div className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        </div>
         <div>
         My name is : {this.state.name}
         My age is : {this.state.age}
@@ -48,34 +55,23 @@ class App extends Component {
         
         
         <div>
-         <form>
-            <label>
-              number1:
-              <input type="integer" name="number1" />
-            </label>
-            <label>
-              number2:
-              <input type="interger" name="number2" />
-            </label>
-            <input type="submit" value="Submit" />
-
-         </form>
-        </div>
-        
-        <div>
-          <Header title= "Test props" />
-          <Header title="Another title" />
-          
-        </div>    
-        <div>
           <button onClick={this.onClick.bind(this)} >Click </button>
           <input onChange = {this.setAge.bind(this)} />
         </div>
+        
         <div>
-          <Calculator />
-        </div>
+          <Switch>
+              <Route path="/" component={Input} exact={true} />
+              <Route path="/level1/:value" component={Output} exact={true} />
+          </Switch>
+         
         </div>
 
+
+
+
+        </div>
+        </BrowserRouter>
     );
   }
 }
